bc50692b9aa5149c9098a71d1e662d5126a12edb
Adolfo Jimenez <eadwulfts@gmail.com>
2023-01-20 18:03:43 -0400

    Fix inheritance in Website model + define methods in Password model + reformat for readability

--- passwords/models.py
+++ passwords/models.py
@@ -2,18 +2,35 @@ from django.db import models
 
 from accounts.models import CustomUser
 
+from hashlib import sha256
+
+
+SMALL_TEXT       =  32
+MEDIUM_TEXT      =  64
+LARGE_TEXT       = 128
+EXTRA_LARGE_TEXT = 256
+
 
 class Password(models.Model):
-    account = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    username = models.CharField(max_length=32)
-    password_hash = models.CharField(max_length=64)
-    website = models.ForeignKey('passwords.Website', on_delete=models.CASCADE)
+    account = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='associated_passwords')
+    username = models.CharField(max_length=SMALL_TEXT)
+    password_hash = models.CharField(max_length=MEDIUM_TEXT)
+    website = models.ForeignKey('passwords.Website', on_delete=models.CASCADE, related_name='associated_passwords')
     add_on = models.DateTimeField(auto_now=True, blank=True, null=True)
     modified_on = models.DateTimeField(blank=True, null=True)
 
+    def get_hash(self, string):
+        return sha256(string.encode('utf-8')).hexdigest()
 
-class Website:
-    name = models.CharField(max_length=64)
-    url = models.SlugField(max_length=64, blank=True, null=True)
-    description = models.CharField(max_length=256, blank=True, null=True)
-    
+    def __str__(self):
+        if len(self.website.name) > 0:
+            output = self.website.name
+        elif len(self.website.url) > 0:
+            output = self.website.url 
+        return f'Password for {output}'
+
+
+class Website(models.Model):
+    name = models.CharField(max_length=MEDIUM_TEXT)
+    url = models.SlugField(max_length=MEDIUM_TEXT, blank=True, null=True)
+    description = models.CharField(max_length=EXTRA_LARGE_TEXT, blank=True, null=True)
