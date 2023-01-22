645374e5501d852aae92c29142e9f032fc13a896
Adolfo Jimenez <eadwulfts@gmail.com>
2023-01-21 21:16:21 -0400

    Alter Password and Website models + Regenerate migrations

--- passwords/models.py
+++ passwords/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 from accounts.models import CustomUser
 
@@ -16,21 +17,21 @@ class Password(models.Model):
     username = models.CharField(max_length=SMALL_TEXT)
     password_hash = models.CharField(max_length=MEDIUM_TEXT)
     website = models.ForeignKey('passwords.Website', on_delete=models.CASCADE, related_name='associated_passwords')
-    add_on = models.DateTimeField(auto_now=True, blank=True, null=True)
+    added_on = models.DateTimeField(auto_now=True, blank=True, null=True)
     modified_on = models.DateTimeField(blank=True, null=True)
 
     def get_hash(self, string):
         return sha256(string.encode('utf-8')).hexdigest()
+    
+    def get_absolute_url(self):
+        return reverse('password_detail', kwargs={'pk': self.pk})
 
     def __str__(self):
-        if len(self.website.name) > 0:
             output = self.website.name
-        elif len(self.website.url) > 0:
-            output = self.website.url 
-        return f'Password for {output}'
+            return output
 
 
 class Website(models.Model):
-    name = models.CharField(max_length=MEDIUM_TEXT)
-    url = models.SlugField(max_length=MEDIUM_TEXT, blank=True, null=True)
+    url = models.URLField(max_length=MEDIUM_TEXT, primary_key=True)
+    name = models.CharField(max_length=MEDIUM_TEXT, blank=True, null=True)
     description = models.CharField(max_length=EXTRA_LARGE_TEXT, blank=True, null=True)
