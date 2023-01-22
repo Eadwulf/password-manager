ec48f4b97940ec9d77e2b4c658c1f5de9dc3929b
Adolfo Jimenez <eadwulfts@gmail.com>
2023-01-21 21:28:56 -0400

    Create Loginview

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,14 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from django.contrib.auth.views import LoginView
 
 from accounts.forms import CustomUserCreationForm
 
 
-class SignUpView(CreateView):
+class AccountsSignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+class AccountsLoginView(LoginView):
+    template_name = 'registration/login.html'
