093bad41c8ed69d70b3bbdad2cbd795b0039d69a
Adolfo Jimenez <eadwulfts@gmail.com>
2023-01-21 21:39:38 -0400

    Add ListView, DetailView and Createview for Password model

--- passwords/urls.py
+++ passwords/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from passwords.views import PasswordListView
+from passwords.views import PasswordListView, PasswordCreateView, PasswordDetailView
 
 
 appname = 'passwords'
 urlpatterns = [
-    path('', PasswordListView.as_view(), name='passwordlist'),
+    path('', PasswordListView.as_view(), name='password_list'),
+    path('create/', PasswordCreateView.as_view(), name='password_create'),
+    path('details/<int:pk>/', PasswordDetailView.as_view(), name='password_detail'),
 ]
--- passwords/views.py
+++ passwords/views.py
@@ -1,9 +1,22 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, DetailView
 
-from passwords.models import Password, Website
+from passwords.models import Password
+
+from passwords.forms import CreatePasswordForm
 
 
 class PasswordListView(ListView):
     model = Password
     context_object_name = 'passwords'
-    template_name = 'passwords/password_listview.html'
+    template_name = 'passwords/list_view.html'
+
+
+class PasswordDetailView(DetailView):
+    model = Password
+    context_object_name = 'password'
+    template_name = 'passwords/detail_view.html'
+
+
+class PasswordCreateView(CreateView):
+    form_class = CreatePasswordForm
+    template_name = 'passwords/create_view.html'
