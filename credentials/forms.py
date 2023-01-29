6ae6682e3bf3671231711b040131b74a733a9faa
Adolfo Jimenez <adolfoj@protonmail.com>
2023-01-28 21:34:16 -0400

    Delete unrequired files

--- credentials/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from credentials.models import Credential
-
-
-class CredentialCreateForm(forms.ModelForm):
-    class Meta:
-        model = Credential
-        fields = '__all__'
