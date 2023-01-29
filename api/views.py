1ae7abf0b557c73348e4839aae81841bdfd6c607
Adolfo Jimenez <adolfoj@protonmail.com>
2023-01-29 09:06:26 -0400

    Add handlers for 'PUT', 'PATCH' & 'DELETE' in 'credential_detail' view function

--- api/views.py
+++ api/views.py
@@ -9,17 +9,31 @@ from .serializers import CredentialSerializer
 
 
 @api_view(['GET', 'POST'])
-def credential_list(request):
+def credential_list(request, format=None):
     if request.method == 'GET':
         serialized_credential = CredentialSerializer(Credential.objects.all(), many=True)
         return Response(serialized_credential.data, status=status.HTTP_200_OK)
     
 
 @api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
-def credential_detail(request, pk):
+def credential_detail(request, pk, format=None):
+    try:
+        credential_obj = Credential.objects.get(pk=pk)
+    except Credential.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
     if request.method == 'GET':
-        try:
-            serialized_credential = CredentialSerializer(Credential.objects.get(pk=pk))
+        serialized_credential = CredentialSerializer(credential_obj)
+        return Response(serialized_credential.data, status=status.HTTP_200_OK)
+
+    if request.method == 'PUT' or request.method == 'PATCH':
+        serialized_credential = CredentialSerializer(credential_obj, data=request.data)
+        if serialized_credential.is_valid():
+            serialized_credential.save()
             return Response(serialized_credential.data, status=status.HTTP_200_OK)
-        except ObjectDoesNotExist:
-            return Response({'msg': f'not matching object found for pk={pk}'}, status=status.HTTP_404_NOT_FOUND)
+        return Response(serialized_credential.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'DELETE':
+        credential_obj.delete()
+        return Response(status.HTTP_204_NO_CONTENT) 
+    
